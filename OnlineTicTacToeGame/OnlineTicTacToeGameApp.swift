3e56a082f082a58ce78ddc932a8f40a2215ee4c4
hamitseyrek <hamitseyrek@gmail.com>
2022-03-14 23:23:46 +0300

    Initial Commit

---
+++ OnlineTicTacToeGame/OnlineTicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineTicTacToeGameApp.swift
+//  OnlineTicTacToeGame
+//
+//  Created by Hamit Seyrek on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlineTicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
