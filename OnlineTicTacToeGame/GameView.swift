8fc4f4f8364d4775c18bee718c9164b97b0ce156
hamitseyrek <hamitseyrek@gmail.com>
2022-03-16 18:37:17 +0300

    start for GameView design for game

--- OnlineTicTacToeGame/GameView.swift
+++ OnlineTicTacToeGame/GameView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct GameView: View {
+    let gridColumns: [GridItem] = [GridItem(.flexible(minimum: 10, maximum: 300)),GridItem(.flexible(minimum: 10, maximum: 300)),GridItem(.flexible(minimum: 10, maximum: 300))]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack {
+                Text("Waiting forthe player")
+                
+                Button {
+                    
+                } label: {
+                    GameButtonView(title: "Quit", backgroundColor: .red)
+                }
+                
+                Text("Loading View")
+                
+                Spacer()
+                
+                VStack {
+                    LazyVGrid(columns: gridColumns) {
+                        ForEach (0..<9) { i in
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(.blue.opacity(0.6))
+                                    .frame(width: geometry.size.width / 3 - 15, height: geometry.size.width / 3 - 15, alignment: .center)
+                                
+                                Image(systemName: "applelogo")
+                                    .resizable()
+                                    .frame(width: geometry.size.width / 9, height: geometry.size.width / 9, alignment: .center)
+                                    .foregroundColor(.white)
+                                    
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 }
 
