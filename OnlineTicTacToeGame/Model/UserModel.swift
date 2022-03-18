3978d3ace0d1b812130323fbff312ee597d2461d
hamitseyrek <hamitseyrek@gmail.com>
2022-03-18 21:48:34 +0300

    save and retrieve userData with AppStorage

---
+++ OnlineTicTacToeGame/Model/UserModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserModel.swift
+//  OnlineTicTacToeGame
+//
+//  Created by Hamit Seyrek on 18.03.2022.
+//
+
+import Foundation
+
+struct UserModel: Codable {
+    var id = UUID().uuidString
+}
--- OnlineTicTacToeGame/ViewModel/GameViewModel.swift
+++ OnlineTicTacToeGame/ViewModel/GameViewModel.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 final class GameViewModel: ObservableObject {
+    @AppStorage("user") private var userData: Data?
+    
     let gridColumns: [GridItem] = [GridItem(.flexible(minimum: 10, maximum: 300)),GridItem(.flexible(minimum: 10, maximum: 300)),GridItem(.flexible(minimum: 10, maximum: 300))]
     
     @Published var game = GameModel(id: UUID(), playerOneID: "player1", playerTwoID: "player2", blockMoveForPlayerID: "player1", winnerID: "", rematchPlayerID: [], moves: Array(repeating: nil, count: 9))
     
+    @Published var currentUser: UserModel!
+    
     private let winPattern: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
+    init() {
+        retriveUser()
+        
+        if currentUser == nil {
+            saveUser()
+        }
+    }
     
     func processPlayerOne(for position: Int) {
         if isSquareOccupied(in: game.moves, forIndex: position) { return }
@@ -33,23 +45,44 @@ final class GameViewModel: ObservableObject {
     }
     
     func isSquareOccupied(in moves: [MoveModel?], forIndex index: Int) -> Bool {
-        moves.contains { moveModel in
-            moveModel?.boardIndex == index
+        moves.contains { elem in
+            elem?.boardIndex == index
         }
     }
     
     func checkForWinCondition(for player1: Bool, in moves: [MoveModel?]) -> Bool {
-        let playerMoves = moves.compactMap { $0 }.filter { element in
-            element.isPlayerOne == player1
-        }
-        let playerPosition = Set(playerMoves.map { $0.boardIndex })
+        let playerMoves = moves.compactMap { $0 }.filter { $0.isPlayerOne == player1 }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
         
-        for pattern in winPattern where pattern.isSubset(of: playerPosition) { true }
+        for pattern in winPattern where pattern.isSubset(of: playerPositions) { return true }
         
         return false
     }
     
     func checkForDraw(in moves: [MoveModel?]) -> Bool {
         return moves.compactMap { $0 }.count == 9
+    }
+    
+    //MARK: - User Object
+    func saveUser() {
+        currentUser = UserModel()
+        
+        do {
+            print("encoding user")
+            userData = try JSONEncoder().encode(currentUser)
+        } catch {
+            print("couldnt same user object")
+        }
+    }
+    
+    func retriveUser() {
+        guard let userData = userData else { return }
+        
+        do {
+            print("decoding user")
+            currentUser = try JSONDecoder().decode(UserModel.self, from: userData)
+        } catch {
+            print("no user data")
+        }
     }
 }
