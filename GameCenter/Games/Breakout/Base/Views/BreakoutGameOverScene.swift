0807b477a94020ac60f731d229b4938202c36ec8
hamitseyrek <hamitseyrek@gmail.com>
2022-04-05 00:52:12 +0300

    added replay to gameoverscene

--- GameCenter/Games/Breakout/Base/Views/BreakoutGameOverScene.swift
+++ GameCenter/Games/Breakout/Base/Views/BreakoutGameOverScene.swift
@@ -10,10 +10,12 @@ import SpriteKit
 
 class BreakoutGameOverScene: SKScene {
     
-    let gameOver = SKSpriteNode (imageNamed: "breakoutTextGameOver")
+    let gameOver = SKSpriteNode(imageNamed: "breakoutTextGameOver")
     
     override func didMove(to view: SKView) {
+        
         backgroundColor = .systemBlue
+        gameOver.size.width = gameOver.size.width / 4
         gameOver.position = CGPoint(x: size.width / 2, y: size.height / 2)
         gameOver.setScale(1.5)
         gameOver.zPosition = 5
@@ -22,7 +24,13 @@ class BreakoutGameOverScene: SKScene {
         let fire = SKEmitterNode (fileNamed: "fire")
         fire?.position = CGPoint(x: gameOver.position.x, y: gameOver.position.y - gameOver.size.height / 2)
         fire?.zPosition = 4
-                                                                                               
         addChild (fire!)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+       let scene = BreakoutGameScene(size: self.size)
+       let transition = SKTransition.flipVertical(withDuration: 2)
+       self.view?.presentScene (scene, transition: transition)
     }
 }
