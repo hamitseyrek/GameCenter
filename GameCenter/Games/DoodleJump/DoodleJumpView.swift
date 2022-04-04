b6c6d3fb97fd250f3888c142b80cadcfc164bc6b
hamitseyrek <hamitseyrek@gmail.com>
2022-04-05 01:59:20 +0300

    refactoring for Breakout

---
+++ GameCenter/Games/DoodleJump/DoodleJumpView.swift
@@ -0,0 +1,20 @@
+//
+//  DoodleJumpView.swift
+//  GameCenter
+//
+//  Created by Hamit Seyrek on 5.04.2022.
+//
+
+import SwiftUI
+
+struct DoodleJumpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DoodleJumpView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoodleJumpView()
+    }
+}
