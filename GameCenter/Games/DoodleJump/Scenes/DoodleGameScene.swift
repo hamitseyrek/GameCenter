669f164c849f2360f50eb5bd8b9b22b94fcf976d
hamitseyrek <hamitseyrek@gmail.com>
2022-04-06 03:05:16 +0300

    add platform and doodle that have physics body

--- GameCenter/Games/DoodleJump/Scenes/DoodleGameScene.swift
+++ GameCenter/Games/DoodleJump/Scenes/DoodleGameScene.swift
@@ -35,6 +35,7 @@ class DoodleGameScene: SKScene, SKPhysicsContactDelegate {
         ground.position = CGPoint(x: size.width / 2, y: 10)
         ground.zPosition = 5
         ground.size.height = ground.size.height / 6
+        ground.size.width = ground.size.width / 6
         ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
         ground.physicsBody?.isDynamic = false
         ground.physicsBody?.allowsRotation = false
@@ -51,7 +52,62 @@ class DoodleGameScene: SKScene, SKPhysicsContactDelegate {
         player.physicsBody?.friction = 0
         player.physicsBody?.angularDamping = 0
         player.setScale(0.5)
+        player.physicsBody?.categoryBitMask = bitmasks.player.rawValue
+        player.physicsBody?.collisionBitMask = 0
+        player.physicsBody?.contactTestBitMask = bitmasks.platform.rawValue
         addChild (player)
         
+        makePlatform()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let contactA: SKPhysicsBody
+        let contactB: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            contactA = contact.bodyA // player
+            contactB = contact.bodyB // platform
+        } else {
+            contactA = contact.bodyB // player
+            contactB = contact.bodyA // platform
+        }
+        
+        if contactA.categoryBitMask == bitmasks.player.rawValue && contactB.categoryBitMask == bitmasks.platform.rawValue {
+            
+            if player.physicsBody!.velocity.dy < 0 {
+                player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 700)
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            player.position.x = location.x
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
+    }
+    
+    func makePlatform() {
+        
+        platform.position = CGPoint(x: size.width / 2, y: size.height / 4)
+        platform.zPosition = 5
+        platform.size.width = platform.size.width
+        platform.size.height = platform.size.height / 4
+        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
+        platform.physicsBody?.isDynamic = false
+        platform.physicsBody?.allowsRotation = false
+        platform.physicsBody?.affectedByGravity = false
+        platform.physicsBody?.categoryBitMask = bitmasks.platform.rawValue
+        platform.physicsBody?.collisionBitMask = 0
+        platform.physicsBody?.contactTestBitMask = bitmasks.player.rawValue
+        addChild (platform)
     }
 }
